134178d74e69fcddbee9d1b5443454a1816f7cad
Cole Britton <cole.britton.126@facebook.com>
2016-04-21 10:49:18 -0700

    This is a test commit

--- Xs and Os/ViewController.swift
+++ Xs and Os/ViewController.swift
@@ -104,7 +104,6 @@ class ViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     
